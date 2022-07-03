15554f1b95392568fec38aca0be7c273b116c861
benma77777 <2533676796@qq.com>
2022-07-03 11:14:39 +0800

    初始化项目

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 7777,
+      host: "localhost",
+    },
+  },
 });
